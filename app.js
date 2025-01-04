39ae6016cd258e4a38e7baca37fa395f4ec26ea0
Vedant-Wedekar <vedantwedekar@gmail.com>
2025-01-04 23:34:23 +0530

    the main file for project

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/user.routes');
+const dotenv = require('dotenv');
+dotenv.config();
+const connectToDB = require('./config/db');
+connectToDB();
+ 
+
+
+// Middleware to parse URL-encoded data (form data)
+app.use(express.urlencoded({ extended: true }));
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Use the user routes
+app.use('/user', userRoutes);
+
+// Start the server on port 3001
+app.listen(3001, () => {
+    console.log('Server is running on http://localhost:3001/user/login');
+});
